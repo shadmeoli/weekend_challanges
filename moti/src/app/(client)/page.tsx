c5cae9bc2d018f010266f4762af0f0033d9c976d
shadmeoli <shadrack@shopzetu.com>
2024-07-21 12:45:18 +0300

    [update] variety details section

--- moti/src/app/(client)/page.tsx
+++ moti/src/app/(client)/page.tsx
@@ -3,10 +3,45 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="bg-gray-100">
+    <main className="h-auto space-y-48 font-primary">
       <HeroSection />
       <CategoriesSection />
+      <VarietySection />
+      <WhyusSection />
     </main>
+  );
+}
+
+function HeroSection() {
+  return (
+    <section className="relative flex h-[80vh] w-full flex-col items-start justify-center overflow-hidden bg-black">
+      <Navbar />
+      <section className="z-10 p-20">
+        <h1 className="z-10 w-[65vw] font-primary text-8xl font-light text-white">
+          Experience is everything with Moti
+        </h1>
+        <div className="flex flex-row items-center justify-start space-x-4">
+          <LinkButton
+            className="flex h-14 flex-row items-center justify-center space-x-2 bg-white px-8 text-black transition-all duration-300 ease-in-out hover:bg-red"
+            text="Buy With Moti"
+            location="/listing"
+          />
+          <LinkButton
+            className="flex h-14 flex-row items-center justify-center space-x-2 bg-white px-8 text-black transition-all duration-300 ease-in-out hover:bg-red"
+            text="Sell With Moti"
+            location="/listing"
+          />
+        </div>
+      </section>
+
+      <Image
+        src="/images/hero.jpg"
+        height={800}
+        width={1920}
+        className="absolute top-0 object-contain opacity-60"
+        alt="Hero"
+      />
+    </section>
   );
 }
 
@@ -62,35 +97,55 @@ function CategoriesSection() {
   );
 }
 
-function HeroSection() {
+function VarietySection() {
   return (
-    <section className="relative flex h-[80vh] w-full flex-col items-start justify-center overflow-hidden bg-black">
-      <Navbar />
-      <section className="z-10 p-20">
-        <h1 className="z-10 w-[65vw] font-primary text-8xl font-light text-white">
-          Experience is everything with Moti
-        </h1>
-        <div className="flex flex-row items-center justify-start space-x-4">
+    <section className="flex h-[70vh] w-full items-center justify-center md:p-10">
+      <div className="relative flex h-full w-[60vw] flex-col justify-center gap-4 md:flex-row">
+        <div className="relative flex w-[60vw] flex-col gap-y-6 p-6">
+          <h1 className="text-5xl">Variety and preference.</h1>
+          <p className="text-balance">
+            At Moti, we understand that every driver is unique. That's why we
+            offer an extensive selection of cars to suit every taste and
+            preference. Whether you're looking for a sleek, modern ride or a
+            classic beauty, we have something for everyone. Explore our diverse
+            range and find the perfect car that matches your style
+          </p>
+
+          <div className="flex h-36 w-full flex-row">
+            <span className="flex h-36 w-48 flex-col justify-center border p-4">
+              <h1 className="text-5xl font-light">300+</h1>
+              <h4 className="text-xl font-light">Sellers</h4>
+            </span>
+            <span className="flex h-36 w-48 flex-col justify-center border p-4">
+              <h1 className="text-5xl font-light">$50M+</h1>
+              <h4 className="text-xl font-light">Transacted</h4>
+            </span>
+          </div>
+
           <LinkButton
-            className="flex h-14 flex-row items-center justify-center space-x-2 bg-white px-8 text-black transition-all duration-300 ease-in-out hover:bg-red"
-            text="Buy With Moti"
-            location="/listing"
-          />
-          <LinkButton
-            className="flex h-14 flex-row items-center justify-center space-x-2 bg-white px-8 text-black transition-all duration-300 ease-in-out hover:bg-red"
-            text="Sell With Moti"
-            location="/listing"
+            className="absolute bottom-6 right-28 flex w-28 items-center justify-center bg-lime p-2 transition-all duration-300 ease-in-out hover:bg-black hover:text-white"
+            text="View All"
+            location="/aboutus"
           />
         </div>
-      </section>
+        <div className="w-full md:w-[40vw]">
+          <Image
+            src="/images/variety_section.jpg"
+            height={800}
+            width={1920}
+            className="object-rights h-full w-full object-cover"
+            alt="Hero"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
 
-      <Image
-        src="/images/hero.jpg"
-        height={800}
-        width={1920}
-        className="absolute top-0 object-contain opacity-60"
-        alt="Hero"
-      />
+function WhyusSection() {
+  return (
+    <section className="flex h-[70vh] w-full flex-col items-center p-10 font-primary">
+      <h1 className="text-5xl">Why Sell/Buy with US?</h1>
     </section>
   );
 }
